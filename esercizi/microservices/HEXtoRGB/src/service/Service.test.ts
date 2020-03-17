81c63f9821b862b5c055cce18e860b3e66063403
Loris De Francisci <loris.defrancisci@itcubeconsulting.it>
2020-03-17 15:04:24 +0100

    Controller + Test

--- esercizi/microservices/HEXtoRGB/src/service/Service.test.ts
+++ esercizi/microservices/HEXtoRGB/src/service/Service.test.ts
@@ -1,13 +1,13 @@
 import chai from 'chai';
 import {convert} from './Service';
-import { rgb2hexTestData } from "../../../commons/src/test-data/colors"
+import { hex2rgbTestData } from "../../../commons/src/test-data/colors"
 
 chai.config.includeStack = true;
 const should = chai.should();
 
 //TODO
 describe('test suite description', () => {
-    rgb2hexTestData.forEach((test) => {
+    hex2rgbTestData.forEach((test) => {
         it(`test case description`, () => {
             convert(test.hexValue).should.deep.equal(test.rgbValue);
         });
